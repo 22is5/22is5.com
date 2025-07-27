547e4b8d6c0199070797b5ea1128e69576977906
Turnstiles <animeister10@gmail.com>
2025-07-27 12:16:12 -0400

    new ui

--- js/randomText.js
+++ js/randomText.js
@@ -1,12 +1,12 @@
- fetch("/js/text.json")
-        .then(response => response.json())
-        .then(data => {
-            const text = data.text;
-            let randomMessage = ""
+fetch("/js/text.json")
+  .then((response) => response.json())
+  .then((data) => {
+    const text = data.text;
+    let randomMessage = "";
 
-                randomMessage = text[Math.floor(Math.random() * text.length)];
+    randomMessage = text[Math.floor(Math.random() * text.length)];
 
-            const randomDiv = document.getElementById('random')
-            randomDiv.innerHTML = randomMessage;
-            document.title = randomMessage;
-        })
+    // const randomDiv = document.getElementById("random");
+    // randomDiv.innerHTML = randomMessage;
+    document.title = randomMessage;
+  });
