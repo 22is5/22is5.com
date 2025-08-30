002deb851027a17c17f5d27bfc28ff15483364c5
Turnstiles <animeister10@gmail.com>
2025-08-30 16:17:42 -0400

    factions are BACK

--- minecraft/tooltips.js
+++ minecraft/tooltips.js
@@ -41,3 +41,70 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 // DISCORD button
+
+// FACTIONS
+document.addEventListener("mousemove", (event) => {
+  const followerDiv = document.getElementById("populist-tooltip");
+  followerDiv.style.left = event.clientX + "px";
+  followerDiv.style.top = event.clientY + "px";
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const bureauElements = document.querySelectorAll(".populist");
+  const tooltip = document.getElementById("populist-tooltip");
+
+  bureauElements.forEach((element) => {
+    element.addEventListener("mouseenter", () => {
+      tooltip.style.display = "block";
+    });
+
+    element.addEventListener("mouseleave", () => {
+      tooltip.style.display = "none";
+    });
+  });
+});
+// populist faction
+
+document.addEventListener("mousemove", (event) => {
+  const followerDiv = document.getElementById("trench-tooltip");
+  followerDiv.style.left = event.clientX + "px";
+  followerDiv.style.top = event.clientY + "px";
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const bureauElements = document.querySelectorAll(".trench");
+  const tooltip = document.getElementById("trench-tooltip");
+
+  bureauElements.forEach((element) => {
+    element.addEventListener("mouseenter", () => {
+      tooltip.style.display = "block";
+    });
+
+    element.addEventListener("mouseleave", () => {
+      tooltip.style.display = "none";
+    });
+  });
+});
+// populist faction
+
+document.addEventListener("mousemove", (event) => {
+  const followerDiv = document.getElementById("bureau-tooltip");
+  followerDiv.style.left = event.clientX + "px";
+  followerDiv.style.top = event.clientY + "px";
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const bureauElements = document.querySelectorAll(".bureau");
+  const tooltip = document.getElementById("bureau-tooltip");
+
+  bureauElements.forEach((element) => {
+    element.addEventListener("mouseenter", () => {
+      tooltip.style.display = "block";
+    });
+
+    element.addEventListener("mouseleave", () => {
+      tooltip.style.display = "none";
+    });
+  });
+});
+// populist faction
