ee54bf7d2185c744288a6474c25fd7a53d30f734
Turnstiles <animeister10@gmail.com>
2025-08-30 08:16:51 -0400

    my magnum opus or something

---
+++ minecraft/click.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const buttons = document.querySelectorAll(".button");
+  const audio = new Audio("audio/click.wav");
+
+  buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      audio.play().catch((error) => {
+        console.error("Error playing sound:", error);
+      });
+    });
+  });
+});
