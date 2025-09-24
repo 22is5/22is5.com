a9cb5c223fbbb6e5e0abc5f2b20590b77b48a90e
Turnstiles <animeister10@gmail.com>
2025-09-24 08:25:18 -0400

    fancy new thing

--- minecraft/pumpkin.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("keydown", function (event) {
-  if (event.key.toLowerCase() === "p") {
-    const pumpkin = document.getElementById("pumpkin");
-    pumpkin.style.display = pumpkin.style.display === "none" ? "block" : "none";
-  }
-});
